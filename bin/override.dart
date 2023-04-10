7d1b479a271663e917e4cac2a980ce97b833da79
Mr-TS-1998 <abhishekts.clt@gmail.com>
2023-04-10 12:45:01 +0530

    new

--- bin/override.dart
+++ bin/override.dart
@@ -4,16 +4,26 @@ class A
   {
     print("Inside a function $d");
   }
+  void show()
+  {
+    print("Another inside function");
+  }
 }
 
 class B extends A
 {
   @override
-  void display(int a) {
+  void display(int a)
+  {
     int b=20, c=50;
     print("$a+$b+$c=${a+b+c}");
     // TODO: implement display
     super.display(1000);
+  }
+  @override
+  void show() {
+    // TODO: implement show
+    super.show();
   }
 }
 
@@ -21,4 +31,5 @@ void main()
 {
   B obj=B();
   obj.display(15);
+  obj.show();
 }
